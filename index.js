df6f0fa2774d288cba89cc56ca0b9d515044c2b9
ysaiteja <saiteja6734.yelagandula@gmail.com>
2023-01-29 17:31:07 +0530

    change

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const dotenv = require('dotenv')
 const app = express();
 const cors = require('cors')
 app.use(cors({origin: '*'}))
+app.use(cors({
+    origin:['https://codefriend.netlify.app'],
+    credentials:true
+}));
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "https://codefriend.netlify.app");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials',true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 app.use(express.json());
 require('./database/connect')
 require('./database/model')
